ba82ffe76d814d15be61719247562b077046fc1b
Henry Charge <henry.charge@valtech.co.uk>
2018-04-30 16:51:53 +0100

    Add edit details page

--- app/routes.js
+++ app/routes.js
@@ -20,4 +20,8 @@ router.get('/admin/create-user', function (req, res) {
   res.render('admin/create-user', {'title':'Create a new user','manage_class':'active'})
 })
 
+router.get('/admin/edit-details', function (req, res) {
+  res.render('admin/edit-details', {'title':'User details','manage_class':'active'})
+})
+
 module.exports = router
