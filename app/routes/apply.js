87a8aa1ef93dc69311aa185192bb03cd64b98829
Sam Quayle <sam.quayle@valtech.co.uk>
2018-07-16 18:02:09 +0100

    Minor content edits based on Michael's review and a new question for those with a disability in both arms

--- app/routes/apply.js
+++ app/routes/apply.js
@@ -525,12 +525,18 @@ router.get('/prove-eligibility/drive-adapted-backend', function(req, res) {
   if (req.session.data['drive-adapted-vehicle'] == 'yes') {
     res.redirect(proveEligibilityPath+'upload-adapted-evidence');
   } else {
-    res.redirect(proveEligibilityPath+'describe-conditions');
+    res.redirect(proveEligibilityPath+'difficulty-with-parking-meters');
   }
 });
 
+router.get('/prove-eligibility/difficulty-with-parking-meters', function(req, res) {
+  res.locals.formAction = '/apply-for-a-blue-badge/prove-eligibility/describe-conditions';
+  res.render(proveEligibilityTemplatePath+'difficulty-parking-meters');
+});
+
+
 router.get('/prove-eligibility/upload-adapted-evidence', function (req, res) {
-  res.locals.formAction = 'describe-conditions';
+  res.locals.formAction = 'difficulty-with-parking-meters';
   res.locals.submitLabel = 'Continue';
   res.locals.change = req.query.change;
   res.render(proveEligibilityTemplatePath+'upload-adapted-evidence')
